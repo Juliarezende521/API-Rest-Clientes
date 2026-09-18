6d60ba7861e979cdf57d0d7fffc0c7ce116b53a0
Julia Rezende <contatojuliagrezende@gmail.com>
2026-09-17 23:00:17 -0300

    Adiciona consultas de verificação de e-mail

--- src/main/java/dc/unifacef/memoria/repository/ClienteRepository.java
+++ src/main/java/dc/unifacef/memoria/repository/ClienteRepository.java
@@ -4,4 +4,8 @@ import dc.unifacef.memoria.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+    boolean existsByEmailIgnoreCase(String email);
+
+    boolean existsByEmailIgnoreCaseAndIdNot(String email, Long id);
 }
